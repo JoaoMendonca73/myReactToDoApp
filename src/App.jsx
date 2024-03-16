4207457e9cb636239654954f63524cac70398ab8
JoaoMendonca73 <miguel.braport@gmail.com>
2024-03-16 16:27:14 -0300

    Added done functionality Need to fix all cards line-through

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
 export default function ToDoApp() {
   const [tasks, setTasks] = useState([]);
   let [newTask, setNewTask] = useState("");
+  const [timeNow, setTimeNow] = useState("00:00");
+  const [isDone, setIsDone] = useState(false);
 
   const handleSetTasks = () => {
     setTasks((t) => [...t, newTask]);
@@ -14,13 +17,38 @@ export default function ToDoApp() {
     setNewTask(e.target.value);
   };
 
+  const handleDoneButton = () => {
+    setIsDone(!isDone)
+  }
+
+  //Delete Button Action
+
   const handleDelButton = (index) => {
     const newTasks = tasks.filter((_, i) => i !== index);
     setTasks(newTasks);
   };
 
+  //Set clock
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      const dt = new Date().toLocaleTimeString();
+
+      setTimeNow(dt);
+    }, 1000);
+
+    // Clear clock interval
+    return () => {
+      clearInterval(int);
+    };
+  }, []);
+
   return (
     <div className="appContainer">
+      <div className="cpy">&copy;2024 Miguel</div>
+      <div className="timeDisplay">
+        <p>{timeNow}</p>
+      </div>
       <h1>My ToDo App</h1>
       <div className="inputContainer">
         <input
@@ -38,9 +66,12 @@ export default function ToDoApp() {
       <ul className="cardContainer">
         {tasks.map((item, i) => {
           return (
-            <li key={i} id={i}>
+            <li key={item.id} id={i} className={isDone?'done':''}>
               <p>{item}</p>
               <span className="btnContainer">
+                <button className="doneButton" onClick={handleDoneButton}>
+                  Done
+                </button>
                 <button
                   className="delButton"
                   onClick={() => handleDelButton(i)}
